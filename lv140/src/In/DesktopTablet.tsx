afe464db2af35245158065d4c572cbcff06c183f
dasago78 <dasago78@DESKTOP-U6C9GOB>
2021-01-16 00:35:43 +0100

    flip unclick

--- lv140/src/In/DesktopTablet.tsx
+++ lv140/src/In/DesktopTablet.tsx
@@ -34,6 +34,15 @@ class DesktopTablet extends React.Component<any, any> {
             itemToFlip: order
         });
     }
+    flipclick(order: number) {
+
+        if (this.state.order === order){
+            order=0;
+        }
+        this.setState({
+            itemToFlip: order
+        });
+    }
     unflip() {
         console.log('unflip');
         this.setState({
@@ -60,7 +69,7 @@ class DesktopTablet extends React.Component<any, any> {
                                     {item.type === "brand" && (
                                         <>
                                             <ReactCardFlip isFlipped={this.state.itemToFlip === item.order ? true : false} flipDirection="horizontal">
-                                                <div onMouseEnter={(event: React.MouseEvent<HTMLElement>) => { this.flip(item.order) }} onClick={(event: React.MouseEvent<HTMLElement>) => { this.flip(item.order) }} className="CardFront" >
+                                                <div onMouseEnter={(event: React.MouseEvent<HTMLElement>) => { this.flip(item.order) }} onClick={(event: React.MouseEvent<HTMLElement>) => { this.flipClick(item.order) }} className="CardFront" >
                                                     <div className="brand-year-desktop-tablet"><h2>{item.year}</h2></div>
                                                     <div className="brand-logo-desktop-tablet"><img className="logo" alt={item.name} title={item.name} src={"./img/logos/" + item.name + ".png"} /></div>
                                                 </div>
